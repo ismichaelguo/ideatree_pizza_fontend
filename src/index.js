70a73b07666d28f78ab2aa1bc6adf433bd13d56d
Michael Guo <ismichaelguo@gmail.com>
2020-06-01 20:08:53 +1000

    add redux-thunk

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import './index.css';
 import App from './App';
 import {Provider} from 'react-redux';
 import rootReducer from './reducers/index';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { devToolsEnhancer } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+
+const middleware = [ thunk ];
 
 const store = createStore(rootReducer, /* preloadedState, */ devToolsEnhancer(
+  applyMiddleware(middleware)
 ));
 
 ReactDOM.render(
